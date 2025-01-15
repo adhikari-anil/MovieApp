76a2f619921acda1a118432168a545d3a15c1719
Anil Adhikari <adhikarianil719@gmail.com>
2025-01-15 23:03:58 +0545

    Proper TV show and Movies Feature with proper scroll feature are added..

--- src/components/layouts/HeadComponent/HeadComponent.tsx
+++ src/components/layouts/HeadComponent/HeadComponent.tsx
@@ -1,17 +1,31 @@
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { useState } from "react";
+import { Outlet } from "react-router-dom";
+import { ScrollArea } from "@/components/ui/scroll-area";
 
 const HeadComponent = () => {
+  const [movie, setToggle] = useState("tvshows");
   return (
-    <div>
-      <Tabs defaultValue="account" className="w-[400px]">
-        <TabsList>
-          <TabsTrigger value="tvseries">Tv Series</TabsTrigger>
-          <TabsTrigger value="movies">Movies</TabsTrigger>
+    <div className="h-screen">
+      <Tabs defaultValue="tvshows" className="h-full flex flex-col">
+        <TabsList className="sticky top-0 z-10 justify-start">
+          <TabsTrigger value="tvshows" onClick={() => setToggle("tvshows")}>
+            TV SHOWS
+          </TabsTrigger>
+          <TabsTrigger value="movies" onClick={() => setToggle("movies")}>
+            MOVIES
+          </TabsTrigger>
         </TabsList>
-        <TabsContent value="tvseries">
-            TVSeries Here
-        </TabsContent>
-        <TabsContent value="movies">Movies Here</TabsContent>
+        <div className="h-[calc(100%-48px)]">
+          <TabsContent
+            value={movie}
+            className="h-full data-[state=active]:h-full"
+          >
+            <ScrollArea className="h-full">
+              <Outlet />
+            </ScrollArea>
+          </TabsContent>
+        </div>
       </Tabs>
     </div>
   );
