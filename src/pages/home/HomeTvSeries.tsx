ba1cde3981642c21570f18cfa8363efb3d380166
Anil Adhikari <adhikarianil719@gmail.com>
2025-01-28 22:59:10 +0545

    Deleted unnecessary comments and successfully made Layout for Series Portion..

--- src/pages/home/HomeTvSeries.tsx
+++ src/pages/home/HomeTvSeries.tsx
@@ -1,70 +1,60 @@
-// import { StreamPlatform } from "@/_components/Dialog";
-// import { tvShows } from "@/constant/TvShows";
-// const HomeTvSeries = () => {
-//   return (
-//     <div className="h-full w-full overflow-hidden">
-//       <div className="h-full overflow-y-auto">
-//         <div className="p-4 space-y-4 flex flex-col gap-4">
-//           {tvShows.map((shows, index) => (
-//             <div key={index} className="flex flex-row h-48 w-full gap-4">
-//               {/* Image container */}
-//               <div className="w-[20%] h-full">
-//                 <img
-//                   src={shows.image}
-//                   alt={shows.name}
-//                   className="w-full h-full object-cover rounded-lg"
-//                 />
-//               </div>
-
-//               {/* StreamPlatform container */}
-//               <div className="w-[80%] h-full">
-//                 <StreamPlatform
-//                   trailerUrl={shows.trailerUrl}
-//                   movieDetails={shows}
-//                 />
-//               </div>
-//             </div>
-//           ))}
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default HomeTvSeries;
-
 import { tvShows } from "@/constant/TvShows";
 import { StreamPlatform } from "@/_components/Dialog";
+import { useCallback, useEffect, useState } from "react";
+import useType from "@/store/useType";
+import {
+  getTopTrendingMovies,
+  getTopTrendingTvShows,
+} from "@/lib/api/Movies/movies";
 const HomeTvSeries = () => {
+  const [topTrending, setTopTrending] = useState<any[]>([]);
+  const type = useType((state) => state.type);
+
+  const getTopTrending = useCallback(async () => {
+    try {
+      if (type === "movies") {
+        const response = await getTopTrendingMovies(1);
+        setTopTrending(response.data.results);
+      } else {
+        const response = await getTopTrendingTvShows(1);
+        setTopTrending(response.data.results);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }, [type]);
+
+  useEffect(() => {
+    getTopTrending();
+  }, [getTopTrending]);
+
   return (
     <div className="h-full w-full overflow-hidden">
       <div className="h-full overflow-y-auto">
         <div className="p-4 space-y-4 flex flex-col gap-4">
-          {tvShows.map((shows, index) => (
-            <div key={index} className="flex flex-row h-96 w-full gap-4">
-              {/* StreamPlatform container */}
-              <div className="w-full h-full">
-                <StreamPlatform
-                  trailerUrl={shows.trailerUrl}
-                  movieDetails={shows}
-                />
-              </div>
+          <div className="flex flex-row h-96 w-full gap-4">
+            {/* StreamPlatform container */}
+            <div className="w-full h-full">
+              <StreamPlatform
+                trailerUrl={tvShows[0].trailerUrl}
+                movieDetails={tvShows[0]}
+              />
             </div>
-          ))}
+          </div>
         </div>
       </div>
       <h1 className="text-white p-2 font-mono text-2xl">Trending Series</h1>
       <div className="p-2 grid grid-cols-5 gap-4 h-fit w-fit">
-        <MovieCard />
-        <MovieCard />
-        <MovieCard />
-        <MovieCard />
-        <MovieCard />
-        <MovieCard />
-        <MovieCard />
-        <MovieCard />
-        <MovieCard />
-        <MovieCard />
+        {topTrending.map((movie, index) => (
+          <MovieCard
+            title={movie.original_title || movie.original_name}
+            date={movie.release_date || movie.first_air_date}
+            rating={movie.vote_average}
+            poster_path={movie.poster_path}
+            type={type}
+            key={index}
+          />
+        ))}
       </div>
     </div>
   );
@@ -72,26 +62,35 @@ const HomeTvSeries = () => {
 
 export default HomeTvSeries;
 
-const MovieCard = () => {
+interface props {
+  title: string;
+  rating: string;
+  date: string;
+  poster_path: string;
+  type: string;
+}
+
+const MovieCard = ({ rating, title, poster_path, date, type }: props) => {
+  const img_path = `https://image.tmdb.org/t/p/original/${poster_path}`;
   return (
     <div className="relative flex flex-col gap-2 w-full">
       <div className="aspect-[3/4] rounded-lg overflow-hidden">
         <img
-          src={tvShows[0].image}
-          alt={tvShows[0].name}
+          src={img_path}
+          alt="Hello World"
           className="w-full h-full transition-all hover:scale-[1.15]"
         />
       </div>
       {/* Favourite section */}
       {/* description */}
-      <div className="flex justify-between gap-4">
-        <p>{tvShows[0].date?.substring(0, 4)}</p>
+      <div className="flex p-2 justify-between gap-4">
+        <p>{date?.substring(0, 4)}</p>
         <span className="rounded-full border border-gray-600 w-fit px-2">
-          Movie
+          {type==="movies"? "Movie" : "Series"}
         </span>
-        <span className="flex items-center">{tvShows[0].rating}</span>
+        <span className="flex items-center">{`${rating}`.substring(0, 3)}</span>
       </div>
-      <p className="w-full text-white p-4">{tvShows[0].name}</p>
+      <p className="w-full text-white p-2">{title}</p>
     </div>
   );
 };
